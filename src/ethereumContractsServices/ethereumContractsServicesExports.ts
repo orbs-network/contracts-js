2d8524df8c502c3bdb253f5f5b09875580e3adbe
Or Lavy <orlavyil@gmail.com>
2020-10-11 15:46:48 +0200

    Adds 'Subscriptions' and 'MonthlySubscriptionService' from 'subscription-ui'

---
+++ src/ethereumContractsServices/ethereumContractsServicesExports.ts
@@ -0,0 +1,7 @@
+// Subscriptions service
+export * from './subscriptionService/ISubscriptionsService';
+export * from './subscriptionService/SubscriptionsService';
+
+// Monthly subscription service
+export * from './monthlySubscriptionPlanService/IMonthlySubscriptionPlanService';
+export * from './monthlySubscriptionPlanService/MonthlySubscriptionPlanService';
