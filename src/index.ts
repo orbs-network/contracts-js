a8d4b51afa637e2ff0ec94ce40bb023ed8653a94
Or Lavy <orlavyil@gmail.com>
2020-10-14 14:53:41 +0200

    Fixes imports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 // Ethereum contracts services
 export * from './ethereumContractsServices/ethereumContractsServicesExports';
 
+// Utility serics
+export * from './utiityServices/utiityServicesExports';
+
 // Utils
 export * from './utils/utilsExports';
