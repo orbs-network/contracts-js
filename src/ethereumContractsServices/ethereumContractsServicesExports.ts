ebc9fe98b47300eeffc7e811feb698360080311a
Or Lavy <orlavyil@gmail.com>
2020-10-19 12:59:38 +0300

    Adds mobnthly subscription plan service constants

--- src/ethereumContractsServices/ethereumContractsServicesExports.ts
+++ src/ethereumContractsServices/ethereumContractsServicesExports.ts
@@ -15,6 +15,7 @@ export * from './subscriptionService/SubscriptionsService';
 // Monthly subscription service
 export * from './monthlySubscriptionPlanService/IMonthlySubscriptionPlanService';
 export * from './monthlySubscriptionPlanService/MonthlySubscriptionPlanService';
+export * from './monthlySubscriptionPlanService/MonthlySubscriptionPlanServiceConstants';
 
 // Guardians service
 export * from './guardiansService/IGuardiansService';
