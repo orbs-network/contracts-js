35de1466540913f0ddfe9f2674c2e9f58fc64379
Or Lavy <orlavyil@gmail.com>
2020-11-03 15:39:58 +0200

    Upgrades deps _ addressses

--- src/ethereumContractsServices/mainnetAddresses.ts
+++ src/ethereumContractsServices/mainnetAddresses.ts
@@ -4,7 +4,7 @@ export const ORBS_MAIN_NET_CONTRACT_ADDRESSES = {
     staking: '0x01D59Af68E2dcb44e04C50e05F62E7043F2656C3',
     monthlySubscriptionPlanContract: '0xD598345F53cF691eb26fBEAb8aF515F9f7500f72',
     subscriptionsContract: '0x7089210F0ba3920C8443E26fb26D6e2f12E5216f',
-    guardiansContract: '0xAB7F3d56Da621Cff1F5646642d7F79f6A201E4eD',
+    guardiansContract: '0x10e441aa45a7fb4230d1370fba3cf98269bd4b5d',
     committeeService: '0x30fa08665be48f14876e4ce2975f67aabfb68a77',
     delegationsService: '0x53d56b4b1EaEd898Be40cF445a715c55dDD6B09C',
 }
