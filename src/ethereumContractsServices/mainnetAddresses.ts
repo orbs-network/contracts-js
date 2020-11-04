8ce407b9010b902cfab3c7c507f9c2b59a73e38d
Or Lavy <orlavyil@gmail.com>
2020-11-04 18:06:56 +0200

    Updates mainnet addresses

--- src/ethereumContractsServices/mainnetAddresses.ts
+++ src/ethereumContractsServices/mainnetAddresses.ts
@@ -1,10 +1,10 @@
 export const ORBS_MAIN_NET_CONTRACT_ADDRESSES = {
     ercToken: '0xff56Cc6b1E6dEd347aA0B7676C85AB0B3D08B0FA',
-    stakingRewardsContract: '0x71D60f980B3ebF432CAA67962b859A12f3b8c5ea',
+    stakingRewardsContract: '0xB5303c22396333D9D27Dc45bDcC8E7Fc502b4B32',
     staking: '0x01D59Af68E2dcb44e04C50e05F62E7043F2656C3',
-    monthlySubscriptionPlanContract: '0xD598345F53cF691eb26fBEAb8aF515F9f7500f72',
-    subscriptionsContract: '0x7089210F0ba3920C8443E26fb26D6e2f12E5216f',
-    guardiansContract: '0x10e441aa45a7fb4230d1370fba3cf98269bd4b5d',
-    committeeService: '0x30fa08665be48f14876e4ce2975f67aabfb68a77',
-    delegationsService: '0x53d56b4b1EaEd898Be40cF445a715c55dDD6B09C',
+    monthlySubscriptionPlanContract: '0xADAdC1f110f7D2f8645d3A4EAC62E7F053a33f1F',
+    subscriptionsContract: '0xD11EFC10cf3A54B12e3F04143070BE3865E7Bb8E',
+    guardiansContract: '0xce97f8c79228c53b8b9ad86800a493d1e7e5d1e3',
+    committeeService: '0x1a4c7891d2d04b2cd413b98bc3283c8d992f5fa7',
+    delegationsService: '0xB97178870F39d4389210086E4BcaccACD715c71d',
 }
